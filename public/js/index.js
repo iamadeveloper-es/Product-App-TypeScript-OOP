06ad1fdfc118a91d2f541675fdabd5c15ec47615
DivthesignDevLab <daniel.verdu@e.craftww.com>
2020-10-27 13:59:59 +0100

    completada funcionalidad

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,66 @@
 "use strict";
-console.log('Yeahhh');
+var Product = /** @class */ (function () {
+    function Product(name, price, year) {
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+    return Product;
+}());
+var UI = /** @class */ (function () {
+    function UI() {
+    }
+    UI.prototype.addProduct = function (newProduct) {
+        var productList = document.querySelector('#product-list');
+        var productContainer = document.createElement('div');
+        productContainer.setAttribute('class', 'product-container');
+        var productTemplate = "\n            <div class=\"product-body text-center\">\n                <strong>Name: " + newProduct.name + "</strong>\n                <strong>Price: " + newProduct.price + "</strong>\n                <strong>Year: " + newProduct.year + "</strong>\n                <button class=\"btn_custom btn_danger btn_delete\" name=\"delete\">Delete</button>\n            </div>\n        ";
+        productContainer.innerHTML = productTemplate;
+        productList.append(productContainer);
+    };
+    UI.prototype.removeProduct = function (btn) {
+        var _a, _b;
+        (_b = (_a = btn.parentElement) === null || _a === void 0 ? void 0 : _a.parentElement) === null || _b === void 0 ? void 0 : _b.remove();
+    };
+    UI.prototype.resetForm = function (node) {
+        node.reset();
+    };
+    UI.prototype.displayMssg = function (mssg, alertClass) {
+        var alert = document.createElement('div');
+        var alertContainer = document.querySelector('.alert-container');
+        alert.setAttribute('class', "alert alert_" + alertClass);
+        alert.innerHTML = mssg;
+        alertContainer.append(alert);
+        setTimeout(function () {
+            alert.remove();
+        }, 2000);
+    };
+    return UI;
+}());
+//DOM
+var form = document.querySelector('#product-form');
+var inptName = document.querySelector('#name');
+var inptPrice = document.getElementById("price");
+var inptYear = document.querySelector('#year');
+var ui = new UI();
+form === null || form === void 0 ? void 0 : form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    if (!inptName.value || isNaN(inptPrice.valueAsNumber) || !Number(inptYear.value)) {
+        ui.displayMssg('Please fill all the inputs', 'warning');
+    }
+    else {
+        var newProduct = new Product(inptName.value, inptPrice.valueAsNumber, Number(inptYear.value));
+        console.log(newProduct);
+        ui.addProduct(newProduct);
+        ui.resetForm(form);
+        ui.displayMssg('Yo add a product successfully!!', 'success');
+        var btnRemove = document.querySelectorAll('.btn_delete');
+        console.log(btnRemove);
+        btnRemove.forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                ui.removeProduct(btn);
+                ui.displayMssg('You deleted a product...', 'danger');
+            });
+        });
+    }
+});
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,78 @@
-console.log('Yeahhh');
+class Product{
+    name:string;
+    price:number;
+    year:number
+    constructor(name:string, price:number, year:number){
+        this.name = name;
+        this.price = price;
+        this.year = year;
+    }
+}
+
+class UI{
+
+    addProduct(newProduct: Product){
+        const productList = document.querySelector('#product-list') as HTMLDivElement;
+        const productContainer = document.createElement('div') as HTMLDivElement;
+        productContainer.setAttribute('class', 'product-container');
+        let productTemplate = `
+            <div class="product-body text-center">
+                <strong>Name: ${newProduct.name}</strong>
+                <strong>Price: ${newProduct.price}</strong>
+                <strong>Year: ${newProduct.year}</strong>
+                <button class="btn_custom btn_danger btn_delete" name="delete">Delete</button>
+            </div>
+        ` as string;
+        productContainer.innerHTML = productTemplate;
+        productList.append(productContainer);
+
+    }
+    removeProduct(btn: Node){
+        btn.parentElement?.parentElement?.remove();
+    }
+    resetForm(node:HTMLFormElement){
+        node.reset();
+    }
+    displayMssg(mssg: string, alertClass: string){
+        const alert = document.createElement('div') as HTMLDivElement;
+        const alertContainer = document.querySelector('.alert-container') as HTMLDivElement;
+        alert.setAttribute('class', `alert alert_${alertClass}`);
+        alert.innerHTML = mssg;
+        alertContainer.append(alert);
+        setTimeout(() => {
+            alert.remove();
+        }, 2000);
+    }
+}
+
+
+//DOM
+const form = document.querySelector('#product-form') as HTMLFormElement;
+const inptName = document.querySelector('#name') as HTMLInputElement;
+const inptPrice = document.getElementById("price") as HTMLInputElement;
+const inptYear = document.querySelector('#year') as HTMLInputElement;
+
+const ui = new UI();
+
+form?.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if(!inptName.value || isNaN(inptPrice.valueAsNumber)|| !Number(inptYear.value)){
+        ui.displayMssg('Please fill all the inputs', 'warning'); 
+    }else{
+        const newProduct = new Product(inptName.value, inptPrice.valueAsNumber, Number(inptYear.value));
+        console.log(newProduct);
+        ui.addProduct(newProduct);
+        ui.resetForm(form);
+        ui.displayMssg('Yo add a product successfully!!', 'success'); 
+        const btnRemove = document.querySelectorAll('.btn_delete') as NodeList;
+        console.log(btnRemove)
+        btnRemove.forEach(btn => {
+            btn.addEventListener('click', () => {
+                ui.removeProduct(btn);
+                ui.displayMssg('You deleted a product...', 'danger')
+            })
+        });
+    }
+    
+})
+
